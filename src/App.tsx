1ff027fac5330af15985d382fb79d67febecaaa7
kiptanui boaz <boaserem022@gmail.com>
2022-12-16 12:34:15 +0300

    added:

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import UserInterface from "../src/UserInterface";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(props:UserInterface) {
+   props={
+    name:"bob",
+    age:12, 
+    address 1133133,
+    dob:22-12-2000
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>User Component</h1>
+      Hello, <b>{props.name}</b>
+      <br />
+      You are <b>{props.age} years old</b>
+      <br />
+      You live at: <b>{props.address}</b>
+      <br />
+      You were born: <b>{props.dob.toDateString()}</b>
     </div>
+
   )
 }
 
