b703d7924ba8931d588b9d651040bc39bf22646e
kiptanui boaz <boaserem022@gmail.com>
2022-12-16 11:09:13 +0300

    add:

---
+++ src/UserInterface.ts
@@ -0,0 +1,8 @@
+export default interface User{
+    name: string;
+    age: number;
+    address: string;
+    dob: Date;
+  }
+//   The code block above defines a simple User interface, which will be passed in as
+  
